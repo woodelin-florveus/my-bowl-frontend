df97c81ab7cd4abea7eb21b16d744601f28567af
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-13 18:55:06 -0500

    add form for meal planner

---
+++ src/components/MealForm.js
@@ -0,0 +1,13 @@
+
+
+const MealForm = () => {
+
+    return(
+        <>
+            this is the meal form add a search bar and more data 
+        </>
+    )
+}
+
+
+export default MealForm;
--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -4,7 +4,12 @@ const MealPlan = () => {
 
     return (
         <>
-            This is the MealPlan page add more to it.
+            <section className="mealPlan__intro">
+                <h1 className="section__title section__title--mealPlan--intro"> Meal Plan </h1>
+                <div className="meal__creator">
+                    <button className="meal_button"> + </button>
+                </div>
+            </section>
         </>
     )
 
