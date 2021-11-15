e6675852a8ea3f1c9cccc4de3942fc30b5987d49
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-15 13:21:22 -0500

    initiated search bar

--- src/components/MealForm.js
+++ src/components/MealForm.js
@@ -1,11 +1,21 @@
-
+import { useState, useEffect } from "react";
 
 const MealForm = () => {
 
     return(
-        <>
-            this is the meal form add a search bar and more data 
-        </>
+        <div className="formMeal">
+
+            <section className="search_meal">
+                <div className="search">
+                    <input type="text"  className="search_food" placeholder="what meals are you looking for"/>
+                    <button
+                    type="submit"
+                    className="search_button"
+                    />
+                </div>
+            </section>
+             
+        </div>
     )
 }
 
