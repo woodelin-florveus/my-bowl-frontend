3f0a076b4ab6ef96a91d4985c9800530f39199fc
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-15 15:54:56 -0500

    added third party api need to pull more data

--- src/components/MealForm.js
+++ src/components/MealForm.js
@@ -2,18 +2,67 @@ import { useState, useEffect } from "react";
 
 const MealForm = () => {
 
+    const [searchMeal, setSearchMeal] = useState('Rice')
+
+
+    const handleChange = (event) => {
+        setSearchMeal(event.target.value)
+    }
+
+    const resetSearch = () => {
+        setSearchMeal("")
+    }
+
+    const callSearch = (event) => {
+        event.preventDefault()
+        setSearchMeal({...searchMeal})
+        resetSearch()
+
+    }
+
+    const submitSearch = (event) => {
+        event.preventDefault()
+
+        fetch(`http//localhost:3000/meals/search`, {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json", 
+                "Accepts": "application/json"
+            }, 
+             body: JSON.stringify({
+                 query: searchMeal
+             })
+        })
+
+    }
+
+
+
     return(
         <div className="formMeal">
 
-            <section className="search_meal">
+
+            <form className="searchForm" onSubmit={submitSearch}>
+                <input
+                    value={searchMeal}
+                    onChange={handleChange}
+                    type="text"
+                />
+                <input onClick={callSearch} type="submit" value="SEARCH" />
+            </form>
+
+
+
+            {/* <section className="search_meal">
                 <div className="search">
                     <input type="text"  className="search_food" placeholder="what meals are you looking for"/>
                     <button
+                    name="search"
                     type="submit"
                     className="search_button"
                     />
                 </div>
-            </section>
+            </section> */}
              
         </div>
     )
