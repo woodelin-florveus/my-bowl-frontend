27707276c56e1262efdce68a6fdd202672fae622
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-16 19:17:30 -0500

    finish mealplan set up

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -1,13 +1,31 @@
+import { useHistory } from "react-router-dom";
 
+import { useState, useEffect } from "react";
 
 const MealPlan = () => {
+
+    const history = useHistory()
+
+    const [mealPlan, setMealPlan] = useState([])
+    
+    //meal plan
+
+    useEffect(() => {
+        fetch('http://localhost:3000/meals')
+        .then(response => response.json())
+        .then(console.log)
+    },[])
+
+    const redirect = () => {
+        history.push('/mealform')
+    }
 
     return (
         <>
             <section className="mealPlan__intro">
-                <h1 className="section__title section__title--mealPlan--intro"> Meal Plan </h1>
+                <h1 className="section__title mealPlan__title"> Meal Plan </h1>
                 <div className="meal__creator">
-                    <button className="meal_button"> + </button>
+                    <button className="meal_button" onClick={redirect}> + </button>
                 </div>
             </section>
         </>
