6a3c898a6146f59062a782b895e65aeee7187514
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-28 00:42:10 -0500

    40% done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,11 @@
 
 const HomePage = () => {
 
+
+
+
+    const dummy_image = "https://dummyimage.com/300x300/000/fff"
+
     return (
         <div className="pageFormat">
             <section className="intro">
@@ -11,28 +16,37 @@ const HomePage = () => {
 
             <section className="meal_services">
                 <div className="services">
-                    <div className="service">
+                    <div className="service_card">
+                        <img src={dummy_image} alt="photo_dump" />
+                        <div className="content">
                         <h3>Meal Plan</h3>
                         <p>In publishing and graphic design, Lorem ipsum is a placeholder 
                     text commonly used to demonstrate the visual form of a 
                     document or a typeface without relying on meaningful content. 
                     Lorem ipsum may be used as a placeholder before final copy is available.</p>
                     </div>
+                    </div>
 
-                    <div className="service">
+                    <div className="service_card">
+                        <img src={dummy_image} alt="photo_dump" />
+                        <div className="content">
                         <h3>Nutrition Info</h3>
                         <p>In publishing and graphic design, Lorem ipsum is a placeholder 
                     text commonly used to demonstrate the visual form of a 
                     document or a typeface without relying on meaningful content. 
                     Lorem ipsum may be used as a placeholder before final copy is available.</p>
                     </div>
+                    </div>
 
-                    <div className="service">
+                    <div className="service_card">
+                    <img src={dummy_image} alt="photo_dump" />
+                        <div className="content">
                         <h3>About</h3>
                         <p>In publishing and graphic design, Lorem ipsum is a placeholder 
                     text commonly used to demonstrate the visual form of a 
                     document or a typeface without relying on meaningful content. 
                     Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                    </div>
                     </div>
                 </div>
             </section>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,14 @@
+import { useState } from "react"
+
 const SearchResults = ({searchMeal}) => {
 
    console.log(searchMeal)
+
+
+   const [limit, setLimit] = useState(8)
+
+
+   const faulty_image = "https://dummyimage.com/300x300/e3dce3/fff.jpg&text=Food+Image+Not+Available"
 
 
    //iterate through array
@@ -8,12 +16,23 @@ const SearchResults = ({searchMeal}) => {
     return (
         <div className="resultContainer">
             <ul className="resultField">
-                { searchMeal.length > 0 && searchMeal.map((result, index) => (
+                { searchMeal.length > 0 && searchMeal.slice(0, limit ? limit : searchMeal.length).map((result, index) => (
                     <li className="result_cards" key={index}>
-                        <img src={result.food.image} alt="food_image" />
+                        {result.food.image ? <img src={result.food.image} alt="food_image" /> : <img src={faulty_image} alt="not_rendered" />}
                         <div className="card_content">
                             <h3>{result.food.label}</h3>
-                            <p> Add protein and servings here</p>
+                            <ul className="nutrition_info">
+                                <h3 className="calories">{result.food.nutrients.ENERC_KCAL} Cal</h3>
+                                <li className="protein">
+                                    <span>Protein:{result.food.nutrients.PROCNT}</span>
+                                </li>
+                                <li className="fat">
+                                    <span>Fat:{result.food.nutrients.FAT}</span>
+                                </li>
+                                <li className="carbs">
+                                    <span>Carbs:{result.food.nutrients.CHOCDF}</span>
+                                </li>
+                            </ul>
                         </div>
                     </li>
                 ))}                
